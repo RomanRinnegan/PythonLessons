a080443d85fa64bfb40c03342b910c2432d02d8c
Roma <mordor.shmordor@gmail.com>
2023-04-28 15:13:42 +0300

    small fix

--- Lesson 1.py
+++ Lesson 1.py
@@ -10,26 +10,26 @@ print("Ваша фраза:", word)
 name = input("Введите ваше имя: ")
 password = input("Введите пароль: ")
 age = int(input("Укажите ваш возраст: "))
-print("Ваши данные для входа в аккаунт:" , "имя -" , name, "пароль -" , password, "возраст -", age)
+print("Ваши данные для входа в аккаунт:", "имя -", name, "пароль -", password, "возраст -", age)
 
 # Задание № 2
 
 seconds = int(input("Введите количество секунд:"))
 hour = seconds//3600
 seconds %= 3600
-min = seconds//60
+minutes = seconds//60
 seconds %= 60
-print(f'{hour:02}', ":", f'{min:02}', ":", f'{seconds:02}')
+print(f'{hour:02}', ":", f'{minutes:02}', ":", f'{seconds:02}')
 
 # Задание 4
 
 proceeds = int(input("Введите сумму выручки:"))
 costs = int(input("Введите сумму расходов:"))
-if proceeds>costs:
+if proceeds > costs:
     workers = int(input("Укажите количесство сотрудников:"))
     profit = proceeds-costs
-    profitability = round(profit/proceeds,2)
-    workers = round(profit/workers,2)
+    profitability = round(profit/proceeds, 2)
+    workers = round(profit/workers, 2)
     print("Финансовый результат: Прибыль.", "Ее величина:", profit)
     print("Рентабильность выручки составила:", profitability)
     print("Прибыль фирмы в расчете на одного сотрудника составила:", workers)
